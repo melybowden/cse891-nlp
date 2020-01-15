ad258fe095e70f3ad3a92ae01867d80d298aef5e
Madison Bowden <59747587+melybowden@users.noreply.github.com>
2020-01-15 13:36:36 -0500

    multiclass perceptron classifier
    top 1 accuracy: 0.7679 for 20 epochs and linear activation
    does not significantly improve for more epochs

--- mc.py
+++ mc.py
@@ -8,25 +8,27 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 newsgroups_train = fetch_20newsgroups(subset='train')
 vectorizer = TfidfVectorizer()
 vectors = vectorizer.fit_transform(newsgroups_train.data)
-
+BIAS = 1
+NUM_EPOCH = 50
 
 class Perceptron(object):
     """
     MultiClass Perceptron classifier that performs 1 vs All classification
     with linear winner takes all activation
     """
-    def __init__(self, dim_input=30, dim_out=20, learning_rate=.001):
-        self.w = np.zeros((dim_out,dim_input,1))
-        # self.w = np.zeros((dim_input,dim_out))
-        self.dim_input = dim_input
-        self.dim_out = dim_out
+    def __init__(self, dim_input=30, dim_out=1, learning_rate=.5):
+        self.w = np.zeros((dim_out,dim_input,1)) # weight vector for n classes
+        self.bias = np.zeros(dim_out) # Biases added, but performed worse, so removed from predict & train
+        self.dim_input = dim_input # number of features
+        self.dim_out = dim_out # n classes
         self.learning_rate = learning_rate
 
     def predict(self, input_array):
-        # y = np.dot(input_array, self.w)
-        # print(y.shape)
-        # input()
-        y = [input_array.dot(self.w[_]) for _ in range(self.dim_out)]
+        """
+        returns max categorization from list of w.x + b for each classifier
+        param: input_array of size 1 x dim_input
+        """
+        y = [self.bias[_] + input_array.dot(self.w[_]) for _ in range(self.dim_out)]
         return np.argmax(y)
 
     def train(self, training_inputs, labels):
@@ -39,9 +41,14 @@ class Perceptron(object):
             y = self.predict(training_inputs[i]) # get prediction for given input
             if y != labels[i]: # update weights only if prediction is wrong
                 self.w[y] = self.w[y] - self.learning_rate*training_inputs[i].transpose()
+                # self.w[y] -= self.learning_rate*BIAS
                 self.w[labels[i]] = self.w[labels[i]] + self.learning_rate*training_inputs[i].transpose()
+                # self.w[labels[i]] += self.learning_rate*BIAS
 
     def test(self, testing_inputs, labels):
+        """
+        prints top 1 accuracy and returns prediction list
+        """
         count_correct = 0
         pred_list = []
         for test_array, label in zip(testing_inputs,labels):
@@ -53,7 +60,7 @@ class Perceptron(object):
         print('Accuracy is '+str(accuracy))
         return np.asarray(pred_list)
 
-
+# MAIN
 from sklearn.datasets import fetch_20newsgroups_vectorized
 # set.data set.target (numbers) set.target_names
 print("importing training and testing data")
@@ -62,11 +69,11 @@ testing = fetch_20newsgroups_vectorized('test')
 train_labels = training.target
 test_labels = testing.target
 n_samples, n_features = training.data.shape
-NUM_EPOCH = 5
+
 print("done vectorizing data")
 print()
 print("beginning training")
-perceptron = Perceptron(n_features)
+perceptron = Perceptron(n_features,len(training.target_names))
 t0 = time()
 for ii in range(NUM_EPOCH):
     perceptron.train(training.data,train_labels)
